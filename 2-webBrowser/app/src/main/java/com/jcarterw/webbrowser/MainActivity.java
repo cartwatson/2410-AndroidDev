6c4174f56750b472b0db6de5e3b0cb06e2918e1c
cartwatson <cartwatson@gmail.com>
2022-09-23 19:40:35 -0600

    rollback

--- 2-webBrowser/app/src/main/java/com/jcarterw/webbrowser/MainActivity.java
+++ 2-webBrowser/app/src/main/java/com/jcarterw/webbrowser/MainActivity.java
@@ -1,62 +1,39 @@
 package com.jcarterw.webbrowser;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.LinearLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.jcarterw.webbrowser.databinding.ActivityMainBinding;
+import com.jcarterw.webbrowser.History;
 
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // init
         History history = new History();
-        final WebView webView = new WebView(this);
-        LinearLayout topLayout = new LinearLayout(this);
-        LinearLayout webLayout = new LinearLayout(this);
-        LinearLayout mainLayout = new LinearLayout(this);
-
-        final LinearLayout.LayoutParams topParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.HORIZONTAL
-        );
-        topLayout.setBackgroundColor(Color.RED);
-        topLayout.setLayoutParams(topParams);
-
-        final LinearLayout.LayoutParams webParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.HORIZONTAL
-        );
-        webLayout.setBackgroundColor(Color.BLACK);
-        webLayout.setLayoutParams(webParams);
-
-        final LinearLayout.LayoutParams mainParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.VERTICAL
-                );
-        mainLayout.setBackgroundColor(Color.BLUE);
-        mainLayout.setLayoutParams(mainParams);
-
+        // create layout
+        LinearLayout layout = new LinearLayout(this);
         // BACK BUTTON -----------------------------------------------------------------------------
         AppCompatButton back = new AppCompatButton(this);
         back.setText("<");
-        final LinearLayout.LayoutParams params0 =
-                new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final LinearLayout.LayoutParams params0 = new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
         back.setLayoutParams(params0);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,52 +46,46 @@ public class MainActivity extends AppCompatActivity {
         // FORWARD BUTTON --------------------------------------------------------------------------
         AppCompatButton forward = new AppCompatButton(this);
         forward.setText(">");
-        final LinearLayout.LayoutParams params1 =
-                new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
         forward.setLayoutParams(params1);
         forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                history.nextCurr();
+                history.nextCurr(); // adjust history
                 // TODO: take you to new site
             }
         });
-
         // ADDRESS BAR -----------------------------------------------------------------------------
+        // TODO: fix size
         final AppCompatEditText addressBar = new AppCompatEditText(this);
-        final LinearLayout.LayoutParams params3 =
-                new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        1);
+        final LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         addressBar.setLayoutParams(params3);
-
         // GO BUTTON -------------------------------------------------------------------------------
         AppCompatButton go = new AppCompatButton(this);
         go.setText("G");
-        final LinearLayout.LayoutParams params2 =
-                new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
         go.setLayoutParams(params2);
-        go.setOnClickListener(new View.OnClickListener() {
+        forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO: set text in address bar when on new page
-                history.appendNode(addressBar.getText().toString());
-                webView.loadUrl(addressBar.getText().toString());
-                webView.setWebViewClient(new WebViewClient());
+                // when pressed add to history
+//                history.appendNode(**ADDRESS**);
+                // TODO: move to next page
             }
         });
 
-        // add everything to the view
-        topLayout.addView(back);
-        topLayout.addView(forward);
-        topLayout.addView(addressBar);
-        topLayout.addView(go);
-        webLayout.addView(webView);
-        mainLayout.addView(topLayout);
-        mainLayout.addView(webLayout);
+        // display content of webpage
+        // TODO: this entire section
 
-        setContentView(mainLayout);
+        // add everything to the view
+        layout.addView(back);
+        layout.addView(forward);
+        layout.addView(addressBar);
+        layout.addView(go);
+//        layout.addView(); // webpage
+
+        setContentView(layout);
     }
 
 }
